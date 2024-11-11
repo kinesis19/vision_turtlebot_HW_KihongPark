24cadba6688fedb3cdea304df42ad077c0d4dc65
kinesis19 <lordkinesis@naver.com>
2024-11-11 22:36:28 +0900

    :construction: Add: cmd_vel debugging
    To get /cmd_vel topic of Turtlebot3

--- vision_turtlebot3_tracing/include/vision_turtlebot3_tracing/main_control.hpp
+++ vision_turtlebot3_tracing/include/vision_turtlebot3_tracing/main_control.hpp
@@ -2,15 +2,18 @@
 #define MAIN_CONTROL_HPP
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include <iostream>
 
 class MainControl : public rclcpp::Node
 {
 public:
     MainControl();
 
-    void run(); // Method of Run
+    void run();
 
 private:
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_cmd_vel_;
 };
 
 #endif // MAIN_CONTROL_HPP
--- vision_turtlebot3_tracing/src/main_control.cpp
+++ vision_turtlebot3_tracing/src/main_control.cpp
@@ -2,13 +2,24 @@
 
 MainControl::MainControl() : Node("main_control")
 {   
-    RCLCPP_INFO(this->get_logger(), "MainControl Node 초기화 완료");
+    // Initialize Publisher
+    publisher_cmd_vel_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+
+    RCLCPP_INFO(this->get_logger(), "MainControl Node Initialize Done!");
+
+    run();
 }
 
+// Main Process
 void MainControl::run()
 {
     while (rclcpp::ok())
     {
-        
+        // Debugging: Check cmd_vel of Turtlebot3
+        auto twist_msg = geometry_msgs::msg::Twist();
+        twist_msg.linear.x = 0.1;
+        std::cout << "x: " << twist_msg.linear.x << " z: " << twist_msg.angular.z << std::endl;
+
+        publisher_cmd_vel_->publish(twist_msg);
     }
 }
