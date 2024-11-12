c5f515035cbc0e026d61d021b136b311658128c0
kinesis19 <lordkinesis@naver.com>
2024-11-12 19:48:45 +0900

    :package: Create: qt_vision_turtlebot3_maze package

---
+++ qt_vision_turtlebot3_maze/include/qt_vision_turtlebot3_maze/qnode.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file /include/qt_vision_turtlebot3_maze/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef qt_vision_turtlebot3_maze_QNODE_HPP_
+#define qt_vision_turtlebot3_maze_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+#ifndef Q_MOC_RUN
+#include <rclcpp/rclcpp.hpp>
+#endif
+#include <QThread>
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+class QNode : public QThread
+{
+  Q_OBJECT
+public:
+  QNode();
+  ~QNode();
+
+protected:
+  void run();
+
+private:
+  std::shared_ptr<rclcpp::Node> node;
+
+Q_SIGNALS:
+  void rosShutDown();
+};
+
+#endif /* qt_vision_turtlebot3_maze_QNODE_HPP_ */
